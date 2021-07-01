14acb1d7313839b7250a0bc4c421a47a4fe07da3
Saptarsi Roy <saptarsiroy12@gmail.com>
2021-07-01 17:14:56 +0530

    added log screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:matchr_docker_app/screens/about_screen.dart';
 import 'package:matchr_docker_app/screens/container_screen.dart';
 import 'package:matchr_docker_app/screens/dockerfile_screen.dart';
 import 'package:matchr_docker_app/screens/home_screen.dart';
+import 'package:matchr_docker_app/screens/log_screen.dart';
 import 'package:matchr_docker_app/screens/login_screen.dart';
 
 void main() => runApp(MyApp());
@@ -15,13 +16,14 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.green.shade200,
         hintColor: Colors.black45,
       ),
-      initialRoute: LoginScreen.id,
+      initialRoute: ContainerScreen.id,
       routes: {
         LoginScreen.id: (context) => LoginScreen(),
         HomeScreen.id: (context) => HomeScreen(),
         ContainerScreen.id: (context) => ContainerScreen(),
         DockerfileScreen.id: (context) => DockerfileScreen(),
         AboutScreen.id: (context) => AboutScreen(),
+        LogScreen.id: (context) => LogScreen(),
       },
     );
   }
---
+++ lib/screens/log_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LogScreen extends StatefulWidget {
+  static const String id = 'LogScreen';
+
+  @override
+  _LogScreenState createState() => _LogScreenState();
+}
+
+class _LogScreenState extends State<LogScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
