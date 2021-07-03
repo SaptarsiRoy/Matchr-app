901ad43bb49899e94da2c0d86f1d3afd0cd547c2
Saptarsi Roy <saptarsiroy12@gmail.com>
2021-07-03 20:11:19 +0530

    changed network helper tags

--- lib/utilities/netwotk_helper.dart
+++ lib/utilities/netwotk_helper.dart
@@ -1,14 +1,14 @@
 import 'package:http/http.dart' as http;
 
 class NetworkHelper {
-  final String url = "http://13.233.164.87/cgi-bin/";
+  final String url = "http://3.108.185.135/cgi-bin/";
   Future<String> runContainer(
       {required String osName,
       required String imgName,
-      required int fromPort,
-      required int toPort}) async {
+      required String fromPort,
+      required String toPort}) async {
     String createContianerUrl = url +
-        'create_container.py?os={osName}&image={imgName}&from={formPort}&to={toPort}';
+        'create_container.py?os=$osName&image=$imgName&from=$fromPort&to=$toPort';
     http.Response response = await http.get(Uri.parse(createContianerUrl));
     if (response.statusCode == 200) {
       String data = response.body;
@@ -24,17 +24,19 @@ class NetworkHelper {
       required String imgTag}) async {
     String createImageUrl =
         url + 'create_image.py?url={imgUrl}&img={imgName}&tag={imgTag}';
-    http.Response response = await http.get(Uri.parse(url));
+    http.Response response = await http.get(Uri.parse(createImageUrl));
     if (response.statusCode == 200) {
       String data = response.body;
       return data;
     } else {
+      print(response.statusCode);
       return "Error in running command";
     }
   }
 
   Future<String> getContainers() async {
-    http.Response response = await http.get(Uri.parse(url));
+    String listContainerUrl = url + 'list_container.py';
+    http.Response response = await http.get(Uri.parse(listContainerUrl));
     if (response.statusCode == 200) {
       String data = response.body;
       return data;
